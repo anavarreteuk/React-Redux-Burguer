cefdc01174337b488ec7ddab04016109b2893712
anavarreteuk <a.navarrete.uk@gmail.com>
2019-01-29 20:02:48 +0000

    added folders and top components in the tree

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import Layout from './components/Layout/Layout'
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
-    );
+    return <div>
+        <Layout>
+          <h1>Hello</h1>
+        </Layout>
+      </div>;
   }
 }
 
