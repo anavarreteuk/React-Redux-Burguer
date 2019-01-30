419c58a5205b7025050abb3a17c85e7d535fe4bd
anavarreteuk <a.navarrete.uk@gmail.com>
2019-01-30 12:55:10 +0000

    added burguer state and function to render ingredients from state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,17 @@ import "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
+  const transformIngredient = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
   return (
     <div className="burger">
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
-          <BurgerIngredient type="bread-bottom" />
+      {transformIngredient}
+      
+      <BurgerIngredient type="bread-bottom" />
     </div>
   );
 };
